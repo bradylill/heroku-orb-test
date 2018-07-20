2f700564f6d8572654139c6912529528e3074d97
Brady Lill <bradylill@gmail.com>
2018-07-20 14:55:27 -0400

    Adding simple hello world nodejs app.

---
+++ index.js
@@ -0,0 +1,7 @@
+const PORT = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+
+express()
+  .get('/', (req, res) => res.send('Hello World!'))
+  .listen(PORT, () => console.log(`Example app listening on port ${PORT}`))
